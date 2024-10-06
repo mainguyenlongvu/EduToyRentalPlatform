25d6a7e7367e32579fc87d0ef1e01039b45d1fea
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2024-10-06 14:37:35 +0700

    Code push git

---
+++ ToyShop.Contract.Services/Interface/IContractService.cs
@@ -0,0 +1,16 @@
+﻿using ToyShop.Core.Base;
+using ToyShop.ModelViews.ContractModelView;
+
+namespace ToyShop.Contract.Services.Interface
+{
+    public interface IContractService
+    {
+        Task<ResponseContractModel> GetContractAsync(string id);
+        Task<BasePaginatedList<ToyShop.Contract.Repositories.Entity.ContractEntity>> GetContractsAsync(int pageNumber, int pageSize);
+        Task CreateContractAsync(CreateContractModel model);
+        Task UpdateContractAsync(string id, UpdateContractModel model);
+        Task DeleteContractAsync(string id);
+
+
+    }
+}
--- ToyShop.Services/DependencyInjection.cs
+++ ToyShop.Services/DependencyInjection.cs
@@ -1,36 +1,20 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using ToyShop.Contract.Repositories.Interface;
-using ToyShop.Repositories.Repositories;
+using ToyShop.Repositories.UOW;
 
 namespace ToyShop.Services
 {
     public static class DependencyInjection
     {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRepositories();
+        }
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
 
-        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddRepository();
-            services.AddAutoMapper();
-            services.AddServices(configuration);
-        }
-        public static void AddRepository(this IServiceCollection services)
-        {
-            services
-               .AddScoped<IUnitOfWork, UnitOfWork>();
-        }
-        private static void AddAutoMapper(this IServiceCollection services)
-        {
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-        }
-        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
-        {
-           
-        }
     }
 }
