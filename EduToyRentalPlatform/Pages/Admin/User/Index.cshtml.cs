8f2b815e5ba4193a0829ec09660e23879dc3cd38
Hoang Truong Minh <truonghoangprovip@gmail.com>
2024-10-09 13:47:11 +0700

    Add: UI of UserManagement, OrderManagement

---
+++ EduToyRentalPlatform/Pages/Admin/User/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToyShop.Repositories.Base;
+using ToyShop.Repositories.Entity;
+
+namespace EduToyRentalPlatform.Pages.Admin.User
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ToyShop.Repositories.Base.ToyShopDBContext _context;
+
+        public IndexModel(ToyShop.Repositories.Base.ToyShopDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ApplicationUser> ApplicationUser { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            //ApplicationUser = await _context.ApplicationUsers.ToListAsync();
+        }
+    }
+}
